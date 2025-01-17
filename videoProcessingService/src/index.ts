f0f507f8b85bd0fad0eb5dadbb313c31caed1c8c
Jason Scott <jcpoems@gmail.com>
2025-01-17 17:49:02 -0500

    Added basic video processing
    Using thunder client to send a post request of where an input
    video is and what to name an output video, then using ffmpeg
    to scale the video to 360p currently.

--- videoProcessingService/src/index.ts
+++ videoProcessingService/src/index.ts
@@ -1,12 +1,34 @@
 import express from "express";
+import ffmpeg from "fluent-ffmpeg";
+
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("hello world");
+
+
+app.post("/process-video", (req, res) => {
+    //get path of input video file from request body 
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    if(!inputFilePath || !outputFilePath){
+        res.status(400).send("Bad request: missing filepaths input or output filepath");
+    }
+    ffmpeg(inputFilePath)
+        .outputOptions("-vf", "scale=-1:360") // turn into 360p
+        .on("end", () => {
+
+            res.status(200).send("Video processing finished successfully.");
+
+        })
+        .on("error", (err) => {
+            console.log(`An error occured: ${err.message}`);
+            res.status(500).send(`Internal server rror: ${err.message}`);
+        })
+        .save(outputFilePath);
 });
-
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Video process service listening at http://localhost:${port}`);
 });
