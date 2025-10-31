223090b6010a542ce720d859d4948af15acf0731
Jason Scott <jcpoems@gmail.com>
2025-10-31 11:39:48 -0400

    Add next.js boilerplate
    Got the google cloud run service to work, compresses files if upload directly to the service,
    working on making the UI in nextjs so users can upload videos to the processed video bucket
    / pull them from that bucket to view them

---
+++ crusher-web-client/app/page.tsx
@@ -0,0 +1,14 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <div className={styles.intro}>
+          <h1>To get started, edit the page.tsx file.</h1>
+
+        </div>
+      </main>
+    </div>
+  );
+}
