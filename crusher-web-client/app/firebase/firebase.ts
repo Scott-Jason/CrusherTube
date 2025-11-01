70cab32acff9fccf99c4fbf087c9ea4555b3afa0
Jason Scott <jcpoems@gmail.com>
2025-11-01 13:08:39 -0400

    Add firebase authenticator
    Added firebase authenticator with googlesignin and added sign out functionality
    depending on whether the user is currently authenticated. Set up boiler plate for
    firestore.

---
+++ crusher-web-client/app/firebase/firebase.ts
@@ -0,0 +1,45 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { 
+    getAuth, 
+    signInWithPopup, 
+    GoogleAuthProvider,
+    onAuthStateChanged,
+    User
+ } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDmZG5lull7VZ02KnHANsuDnpe5afRp7Bg",
+  authDomain: "crusher--tube.firebaseapp.com",
+  projectId: "crusher--tube",
+  appId: "1:923174781789:web:c2bff8cf84f219ba231eb4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+//Initiliaze firebase auth
+const auth = getAuth(app);
+
+/**
+ * Signs the user in with a Google popup.
+ * @returns A promise that resolves with the user's credentials.
+ */
+export function signInWithGoogle() {
+    return signInWithPopup(auth, new GoogleAuthProvider());
+}
+
+
+//signs user out, returns @returns a prommise that resolves when the user signs out
+export function signOut(){
+    return auth.signOut();
+}
+
+/**
+ * Trigger a callback when user auth state changes.
+ * @returns A function to unsubscribe callback.
+ */
+export function onAuthStateChangedHelper(callback: (user: User | null) => void) {
+    return onAuthStateChanged(auth, callback);
+}
--- crusher-web-client/app/navbar/navbar.tsx
+++ crusher-web-client/app/navbar/navbar.tsx
@@ -1,16 +1,34 @@
+'use client';
+
 import Image from "next/image";
 import styles from "./navbar.module.css";
 import Link from "next/link";
 
+import SignIn from "./sign-in"
+import { onAuthStateChangedHelper } from "../firebase/firebase";
+import { useState, useEffect } from "react";
+import { User } from "firebase/auth";
+
+//closure state inside func still maintained after function ends
+
 export default function Navbar(){
+    //Init user state
+    const [user, setUser] = useState<User | null>(null);
+    useEffect( () => {
+        const unsubscribe = onAuthStateChangedHelper((user) => {
+            setUser(user);
+        });
+        //cleanup subscription on unmount
+        return () => unsubscribe();
+    });
+
+
     return (
         <nav className={styles.logo}>
             <Link href="/">
-                
-                <Image width={90} height={80} src="/climb.svg" alt="climb logo"/>
-            
-                
+                <Image width={90} height={80} src="/climb.svg" alt="climb logo"/>                
             </Link>
+            <SignIn user={user}/>
             
         </nav>
 
