ca4202699d3968efb96efcb96d8856189b6fcf6c
Jason Scott <jcpoems@gmail.com>
2025-11-11 20:59:06 -0500

    Format according to linter

--- hosting-api-service/functions/src/index.ts
+++ hosting-api-service/functions/src/index.ts
@@ -27,30 +27,27 @@ export const createUser = functions.auth.user().onCreate((user) => {
 });
 
 export const generateUploadUrl = onCall({maxInstances: 1}, async (request) => {
-    //check if user is authed
-    if (!request.auth){
-        throw new functions.https.HttpsError(
-            "failed-precondition",
-            "The function must be called when authenticated."
-        );
-    }
+  // check if user is authed
+  if (!request.auth) {
+    throw new functions.https.HttpsError(
+      "failed-precondition",
+      "The function must be called when authenticated."
+    );
+  }
 
-    const auth = request.auth;
-    const data = request.data;
-    const bucket = storage.bucket(rawVideoBucketName);
+  const auth = request.auth;
+  const data = request.data;
+  const bucket = storage.bucket(rawVideoBucketName);
 
-    // Generate a unique filename for upload
-    const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+  // Generate a unique filename for upload
+  const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
 
-    // Get a v4 signed URL for uploading file
-    const [url] = await bucket.file(fileName).getSignedUrl({
-      version: "v4",
-      action: "write",
-      expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+  // Get a v4 signed URL for uploading file
+  const [url] = await bucket.file(fileName).getSignedUrl({
+    version: "v4",
+    action: "write",
+    expires: Date.now() + 15 * 60 * 1000, // 15 minutes
   });
 
   return {url, fileName};
-
-
-
 });
