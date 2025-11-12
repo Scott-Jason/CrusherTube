992c0bd57c85d2bedbc0e89d1b3e5da6088f4aea
Jason Scott <jcpoems@gmail.com>
2025-11-11 20:44:31 -0500

    Add unique id for user video uploads

--- hosting-api-service/functions/src/index.ts
+++ hosting-api-service/functions/src/index.ts
@@ -1,12 +1,56 @@
-
+import * as functions from "firebase-functions/v1";
 import {setGlobalOptions} from "firebase-functions";
-import {onRequest} from "firebase-functions/https";
 import * as logger from "firebase-functions/logger";
+import {initializeApp} from "firebase-admin/app";
+import {Firestore} from "firebase-admin/firestore";
+import {Storage} from "@google-cloud/storage";
+import {onCall} from "firebase-functions/v2/https";
+
+setGlobalOptions({maxInstances: 10});
+
+initializeApp();
+const firestore = new Firestore();
+const storage = new Storage();
+
+const rawVideoBucketName = "climbing-raw-videos";
+
+export const createUser = functions.auth.user().onCreate((user) => {
+  const userInfo = {
+    uid: user.uid,
+    email: user.email,
+    photoUrl: user.photoURL,
+  };
+
+  firestore.collection("users").doc(user.uid).set(userInfo);
+  logger.info(`user was created ${JSON.stringify(userInfo)}`);
+  return;
+});
+
+export const generateUploadUrl = onCall({maxInstances: 1}, async (request) => {
+    //check if user is authed
+    if (!request.auth){
+        throw new functions.https.HttpsError(
+            "failed-precondition",
+            "The function must be called when authenticated."
+        );
+    }
+
+    const auth = request.auth;
+    const data = request.data;
+    const bucket = storage.bucket(rawVideoBucketName);
+
+    // Generate a unique filename for upload
+    const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+
+    // Get a v4 signed URL for uploading file
+    const [url] = await bucket.file(fileName).getSignedUrl({
+      version: "v4",
+      action: "write",
+      expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+  });
+
+  return {url, fileName};
 
 
-setGlobalOptions({ maxInstances: 10 });
 
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
 });
