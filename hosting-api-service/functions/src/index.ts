6bde44f4b5d72476e64c4d6cbb39cb631a934655
Jason Scott <jcpoems@gmail.com>
2025-11-17 13:02:44 -0500

    Add way to get videos for display

--- hosting-api-service/functions/src/index.ts
+++ hosting-api-service/functions/src/index.ts
@@ -14,6 +14,23 @@ const storage = new Storage();
 
 const rawVideoBucketName = "climbing-raw-videos";
 
+const videoCollectionId = "videos";
+
+export interface Video {
+  id?: string,
+  uid?: string,
+  filename?: string,
+  status?: "processing" | "processed",
+  title?: string,
+  description?: string
+}
+
+export const getVideos = onCall({maxInstances: 1}, async () => {
+  const snapshot =
+    await firestore.collection(videoCollectionId).limit(10).get();
+  return snapshot.docs.map((doc) => doc.data());
+});
+
 export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {
     uid: user.uid,
